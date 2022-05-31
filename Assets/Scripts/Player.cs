ad5e8669d1478968150a5a8d83cafafcccdd2ed9
Kyosuuke <cli@gaming.tech>
2022-05-31 15:09:52 +0200

    Invulnerability and health bar fix

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,30 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{
+{   
+
+    private GameObject player;
 
     public int maxHealth = 100;
     public int currentHealth;
     public HealthBar healthBar;
+
+    Renderer rend;
+    Color c;
+
     // Start is called before the first frame update
     private void Start()
     {
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
+        player = GameObject.Find("Player");
+        rend = GetComponent<Renderer>();
+        c = rend.material.color;
+
     }
 
-    public void Update()
+    void OnCollisionEnter2D(Collision2D collison)
     {
-         void OnCollisionEnter2D(Collision2D other) 
+        if (collison.collider.CompareTag("Enemy") && currentHealth > 0)
+            StartCoroutine("GetInvulnerable");
         {
-           TakeDamage(10);
+            TakeDamage(20);
         }
+    }
 
-        void TakeDamage(int damage)
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+    }     
+
+    void Update()
+    {
+        if (currentHealth <= 0)
         {
-            currentHealth -= damage;
-
-            healthBar.SetHealth(currentHealth);
+            Destroy(gameObject);
         }
+    }
+
+    IEnumerator GetInvulnerable()
+    {
+        Physics2D.IgnoreLayerCollision (6, 7, true);
+        c.a = 0.5f;
+        rend.material.color = c;
+        yield return new WaitForSeconds (1f);
+        Physics2D.IgnoreLayerCollision (6, 7, false);
+        c.a = 1f;
+        rend.material.color = c;
     }
 }
